04bcbf9a3251b7639250c0c3748dd53ee3c93acb
Matthew Casserly <mcass548@live.kutztown.edu>
2024-04-07 19:18:46 -0400

    Loader_script

---
+++ loader.lua
@@ -0,0 +1,3 @@
+local spr = Sprite(32, 32) -- Creates a new sprite of 32x32 pixels
+spr.filename = "MyNewSprite" -- Names the sprite
+app.activeSprite = spr -- Sets the newly created sprite as the active one
